9e3d7722cb1a7eb3cd456f0eb78f909eeae82708
unknown <1093387079@qq.com>
2018-03-23 21:05:43 +0800

    update code

--- app/email.py
+++ app/email.py
@@ -1,9 +1,11 @@
+from .decorators import async
 from threading import Thread
 from flask import render_template
 from flask_mail import Message
 from app import app, mail
+from flask_babel import _
 
-
+@async
 def send_async_email(app, msg):
     with app.app_context():
         mail.send(msg)
@@ -13,15 +15,25 @@ def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    Thread(target=send_async_email, args=(app, msg)).start()
+    # Thread(target=send_async_email, args=(app, msg)).start()
+    send_async_email(app, msg)
 
 
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
-    send_email('[MoodBit] Reset Your Password',
+    send_email(_('[MoodBit] Reset Your Password'),
                sender=app.config['ADMINS'][0],
                recipients=[user.email],
                text_body=render_template('email/reset_password.txt',
                                          user=user, token=token),
                html_body=render_template('email/reset_password.html',
                                          user=user, token=token))
+
+def follower_notification(followed, follower):
+    send_email(_("[MoodBit] %{username}s is now following you!", username=follower.username),
+               app.config['ADMINS'][0],
+               [followed.email],
+               render_template('email/follower_email.txt',
+                               user=followed, follower=follower),
+               render_template('email/follower_email.html',
+                               user=followed, follower=follower))
--- config.py
+++ config.py
@@ -7,10 +7,16 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    # MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    # MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    # MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_SERVER = 'smtp.qq.com'
+    MAIL_PORT = 25
+    MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['1093387079@qq.com', 'fhd573906@gmail.com']
+    LANGUAGES = ['en', 'es', 'en-US', 'en-GB', 'en-CA']
     POSTS_PER_PAGE = 25
